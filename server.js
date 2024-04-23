493780976f791ca27c0d951ce93b87a4f9b9c4c7
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-04-23 01:21:28 -0500

    File - server.js re-spaced

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-// Import necessary modules
+
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const thoughtRoutes = require('./routes/thoughtRoutes');
 
-// Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3005;
 
-// Middleware
 app.use(express.json());
 
-// Connect to MongoDB
 mongoose.connect('mongodb://localhost/social_network_db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -23,11 +20,9 @@ mongoose.connect('mongodb://localhost/social_network_db', {
         console.error('MongoDB connection error:', err);
     });
 
-// Use routes
 app.use('/api/users', userRoutes);
 app.use('/api/thoughts', thoughtRoutes);
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
